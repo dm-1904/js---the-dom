45d2a074f4b2d5b3c1e9c45a5d2ea09c83de2008
damonryon <damon.ryon@gmail.com>
2024-09-25 12:38:41 -0700

    complete about me page animation

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
 const modalOpen = '[data-open]'
 const modalClose = '[data-close]'
-const isVisible = '.is-visible'
+const isVisible = 'is-visible'
 
 
 const openModal = document.querySelectorAll(modalOpen)
 const closeModal = document.querySelectorAll(modalClose)
+console.log("openModal: ", openModal)
+console.log("closeModal: ", closeModal)
 
 // Full Site Modal "open buttons"
 for (const elm of openModal) {
   elm.addEventListener('click', function() {
     const modalID = this.dataset.open
     document.getElementById(modalID).classList.add(isVisible)
+  })
+}
+
+
+for (const elm of closeModal) {
+  elm.addEventListener('click', function() {
+    this.parentElement.parentElement.classList.remove(isVisible)
   })
 }
